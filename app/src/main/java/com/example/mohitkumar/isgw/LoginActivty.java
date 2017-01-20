063be3a6a34d13e9a46defc306ee590c5600b064
Ashish kumar <ashish05182001@gmail.com>
2017-01-21 01:58:50 +0530

    commit 3

---
+++ app/src/main/java/com/example/mohitkumar/isgw/LoginActivty.java
@@ -0,0 +1,16 @@
+package com.example.mohitkumar.isgw;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Dell on 1/21/2017.
+ */
+
+public class LoginActivty extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
