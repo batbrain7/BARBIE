b084daec1f960c68a87bc30a48a35688dbe185b3
Mohit Kumar <kumar.mohit983@gmail.com>
2017-01-20 23:45:06 +0530

    Ai implemented

--- app/src/main/java/com/example/mohitkumar/isgw/MainActivity.java
+++ app/src/main/java/com/example/mohitkumar/isgw/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.mohitkumar.isgw;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import ai.api.AIListener;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
+
+public class MainActivity extends AppCompatActivity implements AIListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onResult(AIResponse result) {
+        
+    }
+
+    @Override
+    public void onError(AIError error) {
+
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
+
     }
 }
