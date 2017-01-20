790d6eb84be3bc4613b51131681bc3cf4b5543c3
Ashish kumar <ashish05182001@gmail.com>
2017-01-21 01:59:40 +0530

    commit 5

---
+++ app/src/main/java/com/example/mohitkumar/isgw/SplashScreen.java
@@ -0,0 +1,37 @@
+package com.example.mohitkumar.isgw;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+
+/**
+ * Created by Dell on 1/21/2017.
+ */
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splashscreen);
+
+        Thread myThread=new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+
+                        Intent intent=new Intent(getApplicationContext(),SpeechToText.class);
+                        startActivity(intent);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+
+    }
+}
