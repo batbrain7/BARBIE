9ec93f8441ad193b7ade419ad8e0bcf75843701c
Ashish kumar <ashish05182001@gmail.com>
2017-01-22 14:00:02 +0530

    commit 19

--- app/src/main/java/com/example/mohitkumar/isgw/SplashScreen.java
+++ app/src/main/java/com/example/mohitkumar/isgw/SplashScreen.java
@@ -2,12 +2,15 @@ package com.example.mohitkumar.isgw;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.Window;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import java.util.Locale;
 
@@ -15,115 +18,41 @@ import java.util.Locale;
  * Created by Dell on 1/21/2017.
  */
 
-public class SplashScreen extends Activity implements TextToSpeech.OnInitListener{
+public class SplashScreen extends Activity {
 
-    TextView textView;
-
-    String[] arr = {"Hello","Hello this","Hello this is","Hello this is Barbie","Hello this is Barbie your","Hello this is Barbie your virtual",
-            "Hello this is Barbie your virtual home ","Hello this is Barbie your virtual home Assistant"};
-
-    String[] ar =  {"Hello","this","is","barbie","your","virtual","AI","assistant"};
-
-    TextToSpeech tts;
-
+    String uriPath = "android.resource://com.example.mohitkumar.isgw/" + R.drawable.barbie;
+    VideoView videoview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splashscreen);
-
-//        tts = new TextToSpeech(this,this);
-
-        textView = (TextView)findViewById(R.id.text3);
-
-//        Thread myThread=new Thread(){
-//            @Override
-//            public void run() {
-//                try {
-//                    sleep(3000);
-
-             //   new Animate().execute();
-        //speakOut();
-
-        for(int i = 0;i<arr.length;i++) {
-            textView.setText(arr[i]);
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-                        Intent intent=new Intent(getApplicationContext(),SpeechToText.class);
-                        startActivity(intent);
-                        finish();
-
-               // } catch (InterruptedException e) {
-                 //   e.printStackTrace();
-              //  }
-            //}
-        //};
-        //myThread.start();
-
-    }
-
-    private void speakOut() {
-        for(int i =0;i<ar.length;i++) {
-            tts.speak(ar[i], TextToSpeech.QUEUE_FLUSH, null);
-            textView.setText(arr[i]);
-
-        }
-    }
-
-    @Override
-    public void onInit(int status) {
-        if (status == TextToSpeech.SUCCESS) {
-
-            int result = tts.setLanguage(Locale.US);
-
-            if (result == TextToSpeech.LANG_MISSING_DATA
-                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                Log.d("TTS", "This Language is not supported");
-            } else {
-                speakOut();
-            }
-
-        } else {
-            Log.e("TTS", "Initilization Failed!");
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-    class Animate extends AsyncTask<Void,String,String>{
-
-        @Override
-        protected void onProgressUpdate(String... values) {
-            super.onProgressUpdate(values[0]);
-            textView.setText(values[0]);
-        }
+        videoview = (VideoView) findViewById(R.id.video);
 
 
-        @Override
-        protected String doInBackground(Void... params) {
+        Uri uri = Uri.parse(uriPath);
+        videoview.setVideoURI(uri);
 
-            for(int i=0;i<arr.length;i++) {
-                tts.speak(ar[i],TextToSpeech.QUEUE_FLUSH,null);
-                publishProgress(arr[i]);
+        videoview.setMediaController(new MediaController(this));
+
+        videoview.start();
+
+        Thread myThread=new Thread(){
+            @Override
+            public void run() {
                 try {
-                    Thread.sleep(1000);
+
+                    sleep(3000);
+
+                    Intent intent=new Intent(getApplicationContext(),SpeechToText.class);
+                    startActivity(intent);
+                    finish();
+
+
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String aVoid) {
-            super.onPostExecute(aVoid);
-        }
+        };
+        myThread.start();
     }
 }
